97d4e14204355a04b930998d9abf4500335a40c6
nazanin1369 <nazanin1427@gmail.com>
2017-02-28 00:19:42 -0800

    Linear Function

--- src/main.py
+++ src/main.py
@@ -1,3 +1,4 @@
+# pylint: disable=invalid-name
 """
 This script builds and runs a graph with miniflow.
 
@@ -8,33 +9,42 @@ build a network that solves the equation below?
 
 (x + y) + y
 """
-from operator import Add, Mul
+from operator import Add, Mul, Linear
 from utils import topological_sort, forward_pass
 from inputNode import Input
 
 x, y, z = Input(), Input(), Input()
-
-f = Add(x, y, z)
-
-feed_dict = {x: 4, y: 5, z: 10}
-
-sorted_nodes = topological_sort(feed_dict)
-output = forward_pass(f, sorted_nodes)
-
-# NOTE: because topological_sort set the values for the `Input` nodes we could also access
-# the value for x with x.value (same goes for y).
-print("{} + {} = {} (according to miniflow)".format(feed_dict[x], feed_dict[y], output))
-
-
 l, m, n = Input(), Input(), Input()
 
-f = Mul(x, y, z)
+f1 = Add(x, y, z)
+f2 = Mul(x, y, z)
 
-feed_dict = {l: 4, m: 5, n: 10}
+feed_dict1 = {x: 4, y: 5, z: 10}
+feed_dict2 = {l: 4, m: 5, n: 10}
 
-sorted_nodes = topological_sort(feed_dict)
-output = forward_pass(f, sorted_nodes)
+sorted_nodes1 = topological_sort(feed_dict1)
+output1 = forward_pass(f1, sorted_nodes1)
+
+sorted_nodes2 = topological_sort(feed_dict2)
+output2 = forward_pass(f2, sorted_nodes2)
 
 # NOTE: because topological_sort set the values for the `Input` nodes we could also access
 # the value for x with x.value (same goes for y).
-print("{} + {} = {} (according to miniflow)".format(feed_dict[x], feed_dict[y], output))
+print ("{} + {} = {} (according to miniflow - add)".format(feed_dict1[x], feed_dict1[y], output1))
+print ("{} + {} = {} (according to miniflow - mul)".format(feed_dict2[x], feed_dict2[y], output2))
+
+
+inputs, weights, bias = Input(), Input(), Input()
+
+f = Linear(inputs, weights, bias)
+
+feed_dict = {
+    inputs: [6, 14, 3],
+    weights: [0.5, 0.25, 1.4],
+    bias: 2
+}
+
+graph = topological_sort(feed_dict)
+output = forward_pass(f, graph)
+
+print(output)
--- src/operator.py
+++ src/operator.py
@@ -27,3 +27,37 @@ class Mul(Node):
     def forward(self):
         self.value = reduce(lambda x, y: x*y, map(lambda node: node.value, self.inbound_nodes))
 
+
+class Linear(Node):
+    def __init__(self, inputs, weights, bias):
+        Node.__init__(self, [inputs, weights, bias])
+
+        # NOTE: The weights and bias properties here are not
+        # numbers, but rather references to other nodes.
+        # The weight and bias values are stored within the
+        # respective nodes.
+
+    def forward(self):
+        """
+        Set self.value to the value of the linear function output.
+        """
+        inputs = self.inbound_nodes[0].value
+        weights = self.inbound_nodes[1].value
+        self.value = bias = self.inbound_nodes[2].value
+
+        for x, w in zip(inputs, weights):
+            self.value += x * w
+
+
+class LinearMatrix(Node):
+    def __init__(self, X, W, b):
+        # Notice the ordering of the input nodes passed to the
+        # Node constructor.
+        Node.__init__(self, [X, W, b])
+
+    def forward(self):
+        """
+        Set the value of this node to the linear transform output.
+
+        Your code goes here!
+        """
