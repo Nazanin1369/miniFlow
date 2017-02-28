6374fe65567a3411cdc616c6ddee4762b3326461
nazanin1369 <nazanin1427@gmail.com>
2017-02-27 21:02:19 -0800

    Forward Propogation

--- src/add.py
+++ src/add.py
@@ -4,10 +4,11 @@ class Add(Node):
     """
     another subclass of Node, actually can perform a calculation (addition).
     """
-    def __init__(self, x, y):
-        Node.__init__(self, [x, y])
+    def __init__(self, *inputs):
+        Node.__init__(self, list(inputs))
 
     def forward(self):
         """
-        You'll be writing code here in the next quiz!
+        Set the value of this node (`self.value`) to the sum of it's inbound_nodes.
         """
+        self.value = sum(n.value for n in self.inbound_nodes)
---
+++ src/main.py
@@ -0,0 +1,28 @@
+"""
+This script builds and runs a graph with miniflow.
+
+There is no need to change anything to solve this quiz!
+
+However, feel free to play with the network! Can you also
+build a network that solves the equation below?
+
+(x + y) + y
+"""
+
+from node import Node
+from inputNode import Input
+from add import Add
+from utils import *
+
+x, y, z = Input(), Input(), Input()
+
+f = Add(x, y, z)
+
+feed_dict = {x: 4, y: 5, z: 10}
+
+sorted_nodes = topological_sort(feed_dict)
+output = forward_pass(f, sorted_nodes)
+
+# NOTE: because topological_sort set the values for the `Input` nodes we could also access
+# the value for x with x.value (same goes for y).
+print("{} + {} = {} (according to miniflow)".format(feed_dict[x], feed_dict[y], output))
